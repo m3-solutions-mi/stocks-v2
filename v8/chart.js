58d0133b2d4ebffa6587de3e7bddea5f7c01e01b
offsite.ryan <offsite.ryan@~gmail.com>
2026-07-16 14:29:53 -0400

    liquidate button

--- v8/chart.js
+++ v8/chart.js
@@ -641,7 +641,7 @@ class Chart {
 
 
             //* OTHER OPTIONS */
-            this.options_candlestick.stroke.width = [IS_SMALL ? 2.5 : (IS_MEDIUM ? 3 : 4.5), 2];
+            this.options_candlestick.stroke.width = [IS_SMALL ? 2.5 : (IS_MEDIUM ? 3 : 4.5), 2.5];
             this.options_candlestick.yaxis = [
                 {
                     seriesName: 'Gain',
